145e053763663fb4c623d784cbd470487c12d625
Gudny Bjork Gunnarsdottir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-24 16:07:56 +0000

    center the header

--- albums/src/Components/Header.js
+++ albums/src/Components/Header.js
@@ -1,18 +1,31 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 // Skýra componentinn það sama og nafnið á skránni
 const Header = () => {
 	//svipaður syntax og í import
-	const { textStyle } = styles;
+	const { textStyle, viewStyle } = styles; // Velur þau eigindi úr styles sem á nota
+	// hér mætti líka sleppa þessu og nota beint í View propertyinu style = style.viewStyle 
 
-	//textStyle er notað hér sem property á Text
-	return <Text style = {textStyle}>Albums!</Text>;
+	return (
+		//View er utan um Text elementið
+		//textStyle og viewStyle  er notað hér sem property á Text
+		<View style={viewStyle}>
+			<Text style={textStyle}> Albums! </Text>
+		</View>
+	);
 };
 
 const styles = {
+	viewStyle: {
+		backgroundColor: '#F8F8F8',
+		alignItems: 'center',
+		justifyContent: 'center', 
+		height: 60,
+		paddingTop: 15
+	},
 	textStyle: {
 		fontSize: 20
 	}
