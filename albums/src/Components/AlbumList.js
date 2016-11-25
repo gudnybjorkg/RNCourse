6f32fcb8a239b219ae137c3ed3b13cb689bfc9c0
Gudny Bjork Gunnarsdottir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-25 13:01:56 +0000

    albumlist on screen

--- albums/src/Components/AlbumList.js
+++ albums/src/Components/AlbumList.js
@@ -1,14 +1,25 @@
 // Import
-import React from 'react';
+import React, { Components } from 'react'; // til þess að þurfa ekki að yfirskrifa React
 import { View, Text } from 'react-native';
 
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+//const AlbumList = () => { þetta er gamla eins og það væri sem functional component
+//Þarf að importa Component
+// þarf alltaf að útfæra renderfallið þegar átt er við class
+// þegar kallað er á Album list þá mun þetta render fall keyra til að birta á skjá
+class AlbumList extends Components {
+  //kallast automatiskt í þetta áður en það er renderað
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List!!!</Text>
+      </View>
+    );
+  } //notar ekki semikommu í class
+}
 
 export default AlbumList;
