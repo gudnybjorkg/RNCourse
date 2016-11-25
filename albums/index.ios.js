615ca49e65238eeca12b4cda6dced9ebc9e7bf60
Gudny Bjork Gunnarsdottir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-25 11:10:50 +0000

    album list added

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,16 +1,22 @@
 // Index.ios.js - place code here for iOS
 
-// Import a library to help create a component
-import React from 'react';  
-import { AppRegistry } from 'react-native'; 
-import Header from './src/Components/Header'; // Import Header skránni sem á svo að render-a. 
+// Import a library to help create a component, þarf ekki .js endinguna en það má
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/Components/Header';
+import AlbumList from './src/Components/AlbumList';
 
 // Create a component
 const App = () => (
-  //<Text> Some text </Text> // JSX útgáfan sem mun compilast yfir í JS element. 
-  <Header headerText={'Albums!'} />  // sendir inn propertyið headerText með textanum Albums!
+  //<Text> Some text </Text> // JSX útgáfan sem compilast yfir í JS element.
+  // sendir inn propertyið headerText með textanum Albums í header componentið!
+  //Má ekk returna 2 hlutum, ef það eru 2 hlutir sem þarf að skila þarf að setja þa
+  //saman í einn, t.d. view
+  <View>
+    <Header headerText={'Albums!'} />
+    <AlbumList />
+  </View>
 );
 
-//Render it to the device
+//Render it to the device, registerComponent is only used here.
 AppRegistry.registerComponent('albums', () => App);
-
--- albums/src/Components/Header.js
+++ albums/src/Components/Header.js
@@ -4,18 +4,18 @@ import { Text, View } from 'react-native';
 
 // Make a component
 // Skýra componentinn það sama og nafnið á skránni
-const Header = (props) => { //Hægt er að búa til Header með því að senda í það property 
+const Header = (props) => { //Hægt er að búa til Header með því að senda í það property
 	//svipaður syntax og í import
 	const { textStyle, viewStyle } = styles; // Velur þau eigindi úr styles sem á nota
-	// hér mætti líka sleppa þessu og nota beint í View propertyinu style = style.viewStyle 
+	// hér mætti líka sleppa þessu og nota beint í View propertyinu style = style.viewStyle
 
 	return (
 		//View er utan um Text elementið
 		//textStyle og viewStyle  er notað hér sem property á Text
 		//Viljum geta breytt textanum í headernum eftir því hvaða content er á skjánum
-		//Matchar prop og það sem er sent í það. 
+		//Matchar prop og það sem er sent í það.
 		<View style={viewStyle}>
-			<Text style={textStyle}> {props.headerText} </Text> 
+			<Text style={textStyle}> {props.headerText} </Text>
 		</View>
 	);
 };
@@ -25,12 +25,13 @@ const styles = {
 		backgroundColor: '#F8F8F8',
 		alignItems: 'center', //hrizontal
 		justifyContent: 'center', //vertical
-		height: 60, 
-		paddingTop: 15, // hversu neðarlega elementið á að vera miðað við efsta hluta containersins, sniðugt til að koma í veg fyrir að fari ofan í efsta barinn í iOS (netið, klukkam og batteríið)
+		height: 60,
+		paddingTop: 15, // hversu neðarlega elementið á að vera miðað við efsta hluta containersins
 		shadowColor: '#000', //Svartur
-		shadowOffset: {width: 0, height: 2}, // ekkert til hliðanna, sést bara neðst því þetta er efsta contentið á síðunni.
+		// ekkert til hliðanna, sést bara neðst því þetta er efsta contentið á síðunni.
+		shadowOffset: { width: 0, height: 2 },
 		shadowOpacity: 0.2, //Styrkleiki, hversu þéttur skugginn er, skalast frá 0 uppí 1.
-		elevation: 2, // 
+		elevation: 2, //
 		position: 'relative' // staðsetur sig miðað við skjaínn, skalast því með stærri og minni skjám.
 	},
 	textStyle: {
