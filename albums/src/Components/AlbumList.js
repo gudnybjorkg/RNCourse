dbace67177f1249157ae805ec503cdff7c711286
Gudny Bjork Gunnarsdottir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-28 11:21:21 +0000

    albums to class entity

--- albums/src/Components/AlbumList.js
+++ albums/src/Components/AlbumList.js
@@ -1,19 +1,24 @@
 // Import
-import React from 'react'; // til þess að þurfa ekki að yfirskrifa React
+import React, { Component } from 'react'; // til þess að þurfa ekki að yfirskrifa React
 import { View, Text } from 'react-native';
 
 
-const AlbumList = () => { //þetta er gamla eins og það væri sem functional component
+//const AlbumList = () => { //þetta er gamla eins og það væri sem functional component
 //Þarf að importa Component
 // þarf alltaf að útfæra renderfallið þegar átt er við class
 // þegar kallað er á Album list þá mun þetta render fall keyra til að birta á skjá
-//class AlbumList extends Component {
+class AlbumList extends Component {
   //kallast automatiskt í þetta áður en það er renderað
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-}; //notar ekki semikommu í class
+  componetWillMount() {
+    console.log('componetWillMount in AlbumList');
+  }
+  render() {
+    return (
+      <View>
+        <Text>Album List!!!</Text>
+      </View>
+    );
+  }
+} //notar ekki semikommu í class
 
 export default AlbumList;
